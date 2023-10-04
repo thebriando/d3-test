660a2f1b43c31c93c011d6dc64de4f47dd3bb65f
Brian Do <thebriando@gmail.com>
2023-10-04 14:45:10 -0700

    Setup reproduction

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import * as d3 from "d3";
+
+const App = () => {
+  console.log(d3.select("body"));
+  return <h1>hi</h1>;
+};
+
+export default App;
---
+++ README.md
@@ -0,0 +1,5 @@
+# d3-test
+
+- Run `yarn build`
+- `cd dist`
+- `http-server -p 3000 --cors -c-1`
